a7f78c5a78a9016e46c7da735cfce0ef4f7642bf
Mikhail_Bakhirev <7074033@gmail.com>
2019-09-24 01:22:57 +0300

    delete_notes_async

---
+++ scripts/create_preview.js
@@ -0,0 +1,13 @@
+function CreatePreview() {
+  const asidePreview = document.querySelector('.aside');
+  const xhrpre = new XMLHttpRequest();
+  xhrpre.open('GET', 'notes_preview.php', true);
+  xhrpre.send();
+  xhrpre.onreadystatechange = function () {
+    if (xhrpre.readyState === XMLHttpRequest.DONE && xhrpre.status === 200) {
+      var previewItem = (this.responseText);
+      asidePreview.insertAdjacentHTML('beforeend', previewItem);
+    };
+    DeleteButton();
+  };
+};
---
+++ scripts/note_delete.js
@@ -0,0 +1,20 @@
+function DeleteButton() {
+  const delButtons = document.querySelectorAll('.preview__btn-del');
+  for (let delButtonIndex = 0; delButtonIndex < delButtons.length; delButtonIndex++) {
+    delButton = delButtons[delButtonIndex];
+    delButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      let currIndex = delButtonIndex;
+      let previewItem = this.parentNode;
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'note_delete.php', true);
+      console.log(currIndex);
+      xhr.send(currIndex);
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+          previewItem.remove();
+        };
+      };
+    });
+  };
+};
