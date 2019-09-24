b16a21d21ea893c3638ff5ecd6e3492d514ff052
Mikhail_Bakhirev <7074033@gmail.com>
2019-09-24 19:11:59 +0300

    tasks_fields

--- scripts/note_send.js
+++ scripts/note_send.js
@@ -1,49 +1,59 @@
-// function SendButton() {
+const form = document.querySelector('.note__form');
+const sendButton = document.querySelector('.note__submit');
 
-  const form = document.querySelector('.note__form');
-  const sendButton = document.querySelector('.note__submit');
+var sendFunc = function (event) {
+  event.preventDefault();
 
 
-  var sendFunc = function (event) {
-    event.preventDefault();
-    if (validateForm(form)) {
-      var currentNote = {
-        head: form.elements.note_head.value,
-        date: form.elements.note_date.value,
-        text: form.elements.note_text.value
-      }
-      const xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-      xhr.open('POST', 'note_add.php');
-      xhr.send(JSON.stringify(currentNote));
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-          form.reset();
-          CreateLastPrewiew();
-        };
-      };
+
+  // if (validateForm(form)) {
+    var currentNote = {
+      head: form.elements.note_head.value,
+      date: form.elements.note_date.value,
+      text: form.elements.note_text.value
     };
-
-
-    function validateForm(form) {
-      let valid = true;
-      if (!validateField(form.elements.note_head)) {
-        valid = false;
+    if (currentNote.head !== '' && currentNote.text !== '') {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('POST', 'note_add.php');
+    xhr.send(JSON.stringify(currentNote));
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        form.reset();
+        CreateLastPrewiew();
       };
-      if (!validateField(form.elements.note_date)) {
-        valid = false;
-      };
-      if (!validateField(form.elements.note_text)) {
-        valid = false;
-      };
-      return valid;
-    };
-
-    function validateField(field) {
-      field.nextElementSibling.textContent = field.validationMessage;
-      return field.checkValidity();
     };
   };
+};
 
-  sendButton.addEventListener('click', sendFunc)
+// const taskFields = document.querySelectorAll('.task');
+
+// var sendFuncTask = function FuncTask() {
+
+
+
+// };
+
+//   function validateForm(form) {
+//     let valid = true;
+//     if (!validateField(form.elements.note_head)) {
+//       valid = false;
+//     };
+//     if (!validateField(form.elements.note_date)) {
+//       valid = false;
+//     };
+//     if (!validateField(form.elements.note_text)) {
+//       valid = false;
+//     };
+//     return valid;
+//   };
+
+//   function validateField(field) {
+//     field.nextElementSibling.textContent = field.validationMessage;
+//     return field.checkValidity();
+//   };
+// };
+
+sendButton.addEventListener('click', sendFunc);
+// sendButton.addEventListener('click', sendFuncTask);
 // };
